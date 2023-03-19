b964948baabe7e9349a8123a97a6a53daf7e4a3d
Jadzia <thymetwisted@gmail.com>
2023-03-19 09:33:50 -0400

    add korean

--- Contents/mods/buffysroleplaychat/media/lua/client/Context/Inventory/InvContextLanguageBooks.lua
+++ Contents/mods/buffysroleplaychat/media/lua/client/Context/Inventory/InvContextLanguageBooks.lua
@@ -7,7 +7,7 @@ function ISInventoryMenuElements.ContextLanguageBooks(context)
     self.invMenu = ISContextManager.getInstance().getInventoryMenu();
     local player = getPlayer()
     local modData = player:getModData()
-    local languages = { "Spanish", "Russian", "Ukrainian", "German", "French", "Mandarin", "Japanese", "ASL" }
+    local languages = { "Spanish", "Russian", "Ukrainian", "German", "French", "Mandarin", "Japanese", "ASL", "Korean" }
     function self.init()
     end
 
