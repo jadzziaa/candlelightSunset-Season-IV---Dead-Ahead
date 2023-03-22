016e1ac70b2f9e70c737ab2f5f6ddce268d22316
Jadzia <thymetwisted@gmail.com>
2023-03-22 12:38:03 -0400

    unwanted mods removal

--- Contents/mods/08sable/media/lua/shared/08sableIcons.lua
+++
@@ -1,8 +0,0 @@
-ContainerButtonIcons = ContainerButtonIcons or {}
-
-local t = {}
-t.spare = getTexture("media/textures/sable_spare.png")
-t.console = getTexture("media/textures/sable_centerConsole.png")
-
-ContainerButtonIcons.sableSpare = t.spare
-ContainerButtonIcons.CenterConsole = t.console
