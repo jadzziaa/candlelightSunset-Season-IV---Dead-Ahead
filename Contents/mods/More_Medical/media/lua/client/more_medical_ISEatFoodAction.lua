59a187ad9f2eaba7b0b49794a3827f85673a432a
Kyaace <Kyaace@gmail.com>
2023-04-03 18:17:53 -0400

    Removing duplicate mod files
    Removing any folder that is suspected to be an unchanged duplicate from SteamWorkshop to prevent having to maintain a potential conflict point for where to get those mods.

--- Contents/mods/More_Medical/media/lua/client/more_medical_ISEatFoodAction.lua
+++
@@ -1,8 +0,0 @@
-local oldISEatFoodAction_adjustMaxTime = ISEatFoodAction.adjustMaxTime
-function ISEatFoodAction:adjustMaxTime(maxtime)
-    if string.sub(self.item:getFullType(), 0, 19) == "MoreMedical.Syrette" then
-        return 100
-    else
-        return oldISEatFoodAction_adjustMaxTime(self, maxtime)
-    end
-end
